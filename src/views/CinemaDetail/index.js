92f77bb11aff8e72ebc535683235ec4f8ce8aa89
AnnaRagnheidur <annarg22@ru.is>
2023-12-14 09:34:09 +0000

    address works

--- src/views/CinemaDetail/index.js
+++ src/views/CinemaDetail/index.js
@@ -3,22 +3,27 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import MovieList from '../../components/MovieList';
 
-const CinemaDetail = ({ 
-  route
-}) => {
-  const { cinema } = route.params;
-  const movies = useSelector((state) => {
-    return state.movie.movies;
+const normalizeKeys = (obj) => {
+  const newObj = {};
+  Object.keys(obj).forEach(key => {
+    const normalizedKey = key.trim();
+    newObj[normalizedKey] = obj[key];
   });
+  return newObj;
+};
 
+const CinemaDetail = ({ route }) => {
+  const { cinema } = route.params;
+  const normalizedCinema = normalizeKeys(cinema);
+  const movies = useSelector((state) => state.movie.movies);
+
+  console.log( cinema )
   return (
     <ScrollView>
       <View style={styles.cinemaContainer}>
         <Text style={styles.cinemaTitle}>{ cinema['name'] }</Text>
         <Text style={styles.cinemaParagraph}>{ cinema['description'] ? cinema['description'].replace(/<br>/g, '').replace(/<b>/g, '') : cinema['description'] }</Text>
-        <Text style={styles.cinemaParagraph}>{ cinema['address\t'] }, { cinema['city'] } </Text>
-        <Text style={styles.cinemaParagraph}>{ cinema['phone'] }</Text>
-        <Text style={styles.cinemaParagraph}>{ cinema['website'] }</Text>
+        <Text style={styles.cinemaParagraph}>{normalizedCinema.address}, {normalizedCinema.city} </Text>        <Text style={styles.cinemaParagraph}>{ cinema['website'] }</Text>
       </View>
       <View style={styles.movieContainer}>
         <Text style={styles.movieTitle}>Movies</Text>
